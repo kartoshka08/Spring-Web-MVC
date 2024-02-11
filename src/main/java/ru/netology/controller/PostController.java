a4b67fbc1b306d33d403aa66a2faa1bb51897471
Kartoshka <kartoshka0811@gmail.com>
2024-02-11 14:44:41 +0300

    update

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,19 +1,14 @@
 package ru.netology.controller;
 
-import com.google.gson.Gson;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/posts")
 public class PostController {
-  public static final String APPLICATION_JSON = "application/json";
   private final PostService service;
 
   public PostController(PostService service) {
@@ -21,29 +16,22 @@ public class PostController {
   }
 
   @GetMapping
-  public void all(HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var data = service.all();
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
+  public List<Post> all() {
+    return service.all();
   }
 
   @GetMapping("/{id}")
-  public void getById(@PathVariable long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
+  public Post getById(@PathVariable long id) {
+    return service.getById(id);
   }
 
   @PostMapping
-  public void save(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
-    final var post = gson.fromJson(request.getReader(), Post.class);
-    final var data = service.save(post);
-    response.getWriter().print(gson.toJson(data));
+  public Post save(@RequestBody Post post) {
+    return service.save(post);
   }
 
   @DeleteMapping("/{id}")
-  public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
+  public void removeById(long id) {
+    service.removeById(id);
   }
 }
